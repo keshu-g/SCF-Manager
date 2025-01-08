3288f62ba8deb96b0446d378eae499f249a7c415
keshu-g <kartikgoswami082@gmail.com>
2025-01-09 00:56:29 +0530

    added products

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = Router();
 import userRouter from "./user.route.js";
 import materialRouter from "./material.route.js";
 import clientRouter from "./client.route.js";
+import productRouter from "./product.route.js";
 
 router.use("/users", userRouter);
 router.use("/materials", materialRouter);
 router.use("/clients", clientRouter);
+router.use("/products", productRouter);
 
 export default router;
---
+++ validations/product.validation.js
@@ -0,0 +1,30 @@
+import joi from "joi";
+import {
+  stringValidation,
+  idValidation,
+  numberValidation,
+} from "../utils/joiValidationHelper.js";
+
+const getProductSchema = joi.object({
+  id: idValidation,
+});
+
+const addProductSchema = joi
+  .object({
+    client: idValidation,
+    name: stringValidation,
+    price: numberValidation,
+    formula: joi.array().items(
+      joi.object({
+        material: idValidation,
+        quantity: numberValidation,
+      })
+    ),
+  })
+  .unknown(false);
+
+const updateProductSchema = addProductSchema.keys({
+  id: idValidation,
+});
+
+export { getProductSchema, addProductSchema, updateProductSchema };

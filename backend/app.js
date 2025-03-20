a62389ceb6b45f03c8ff6e3075d8f924a1aa43c4
keshu-g <kartikgoswami082@gmail.com>
2025-03-20 15:40:55 +0530

    fixed cors issues

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,17 @@ import routes from "./routes/index.js";
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: constants.CORS_ORIGIN }));
+// app.use(cors({ credentials: true, origin: constants.CORS_ORIGIN }));
+app.use(cors({
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl)
+    console.log(origin);
+    if (!origin) return callback(null, true);
+    return callback(null, origin); // Reflect the request origin
+  },
+  credentials: true, // Allow credentials (cookies, headers)
+}));
+
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(express.static("public"));

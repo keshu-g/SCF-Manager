c3a9ccd64f52d63c8404c026b1fe1c3361fb3b30
keshu-g <kartikgoswami082@gmail.com>
2025-04-07 00:36:47 +0530

    added charts in product page

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,12 +1,9 @@
 import { useParams } from "react-router-dom";
-import {
-  useGetProductsQuery,
-  useCreateProductMutation,
-  useUpdateProductMutation,
-  useDeleteProductMutation,
-  useGetProductsByClientIdQuery,
-} from "../features/product/productApi";
+import { useGetProductsByClientIdQuery } from "../features/product/productApi";
 import LoadingScreen from "@/components/loading-screen";
+import { BarChartMixed } from "@/components/product-chart";
+import { LucideEdit, Trash2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const Products = () => {
   const { clientId } = useParams();
@@ -26,14 +23,78 @@ const Products = () => {
     });
   }
 
-  return (
-    <div className="container mx-auto h-full px-4">
-      <h1 className="text-2xl font-bold">Products for Client ID: {clientId}</h1>
-      <p>
-        This is a placeholder page for displaying products of a client.{" "}
+  if (!products.data?.length) {
+    return (
+      <p className="flex flex-wrap gap-4 justify-center p-4 text-2xl">
+        No products found.
       </p>
-      <p>{JSON.stringify(products) || "No products found"}</p>
+    );
+  }
 
+  return (
+    <div className="flex flex-wrap gap-4 p-4 justify-center md:justify-normal">
+      {products.data.map((product, idx) => {
+        const chartData = product.formula.map((item) => ({
+          material: item.material.name,
+          quantity: item.quantity,
+          fill: `var(--color-${item.material.name
+            .toLowerCase()
+            .replace(/\s/g, "")})`,
+        }));
+
+        const chartColors = Array.from({ length: 10 }, (_, i) => i + 1).sort(
+          () => 0.5 - Math.random()
+        );
+
+        const chartConfig = {
+          quantity: { label: "Quantity" },
+        };
+
+        chartData.forEach((item, i) => {
+          const colorIndex = chartColors[i % chartColors.length];
+          chartConfig[item.material.toLowerCase().replace(/\s/g, "")] = {
+            label: item.material,
+            color: `var(--chart-${colorIndex})`,
+          };
+        });
+
+        return (
+          <BarChartMixed
+            key={product._id}
+            xAxisDataKey="quantity"
+            yAxisDataKey="material"
+            title={
+              <div className="flex items-center justify-between gap-2">
+                <p className="text-xl font-semibold">{product.name}</p>
+                <div className="flex gap-2">
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="text-blue-500"
+                  >
+                    <LucideEdit />
+                  </Button>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="text-red-500"
+                  >
+                    <Trash2 />
+                  </Button>
+                </div>
+              </div>
+            }
+            data={chartData}
+            config={chartConfig}
+            footer={
+              <div className="flex justify-evenly items-center w-full">
+                <Button variant="ghost">Weight 50Kg</Button>
+                <Button variant="ghost">Price: ₹{product.price}</Button>
+              </div>
+            }
+          />
+        );
+      })}
     </div>
   );
 };

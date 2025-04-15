f69648f80e9711a57d661f18ca92494a073ac401
keshu-g <kartikgoswami082@gmail.com>
2025-04-15 23:35:16 +0530

    removed test stuff from dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,22 +4,7 @@ import { ProductCard } from "@/components/product-card";
 const Dashboard = () => {
   return (
     <div className="flex flex-wrap gap-4 p-4 w-full">
-      <ProductCard
-        productName="Cool Product"
-        materials={[
-          { name: "Steel", price: 50, quantity: 10 },
-          { name: "Plastic", price: 20, quantity: 5 },
-        ]}
-        otherCosts={[
-          { name: "Transport", amount: 100 },
-          { name: "Packaging", amount: 50 },
-        ]}
-        sellingPrice={4000}
-      />
-
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      More to come...
     </div>
   );
 };

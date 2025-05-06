7e5146026720f2be276c9769d35a263e8a8a9cfa
keshu-g <kartikgoswami082@gmail.com>
2025-05-06 16:22:33 +0530

    Code format

--- backend/routes/transaction.route.js
+++ backend/routes/transaction.route.js
@@ -1,7 +1,13 @@
 import express from "express";
 import { validate } from "../middlewares/validation.middleware.js";
-import { manufactureProduct, materialTransaction } from "../controllers/transaction.controller.js";
-import { manufactureProductSchema, materialTransactionSchema } from "../validations/transaction.validation.js";
+import {
+  manufactureProduct,
+  materialTransaction,
+} from "../controllers/transaction.controller.js";
+import {
+  manufactureProductSchema,
+  materialTransactionSchema,
+} from "../validations/transaction.validation.js";
 
 const router = express.Router();
 

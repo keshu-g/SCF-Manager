aefeb71107c87b67ee7c30dfe95504510c176708
Kartik Goswami <96482872+keshu-g@users.noreply.github.com>
2025-08-19 15:37:35 +0530

    Updated Backend URL

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 // const API_BASE_URL = "http://localhost:5000/api/v1";
 // const API_URL = "http://localhost:5000";
-const API_BASE_URL = "https://scf-manager.onrender.com/api/v1";
-const API_URL = "https://scf-manager.onrender.com";
+const API_BASE_URL = "https://api.scf.kartikgoswami.in/api/v1";
+const API_URL = "https://api.scf.kartikgoswami.in/";
 
 export { API_BASE_URL, API_URL };
